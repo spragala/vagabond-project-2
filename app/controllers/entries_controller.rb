ec31b060de807d7f4555696ce092e8a23ecdf51f
nuranned <nuranned@gmail.com>
2017-07-07 11:25:26 -0700

    added delete functionality for the entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,23 +2,39 @@ class EntriesController < ApplicationController
   def index
     @entries = Entry.all
   end
+
   def new
     @entry = Entry.new
   end
+
   def edit
     @entry = Entry.find(params[:id])
   end
+
   def show
     @entry = Entry.find(params[:id])
   end
+
   def update
     @entry = Entry.find(params[:id])
         if @entry.update entry_params
           redirect_to @entry
-
         end
-
   end
+
+  def create
+    @entry = current_user.entries.build(entry_params)
+    if @entry.save
+      redirect_to @entry
+    end
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
+  end
+
   private
 
      def entry_params

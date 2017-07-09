e683bc1411c3a6aacfceedf31cbc8a9a8a9d4188
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-07-08 22:16:10 -0700

    modified urls to be more user-friendly using friendly_id gem

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,7 +2,9 @@ class City < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_many :users, through: :entries
 
-  def to_param
-    "#{id} #{city}".parameterize
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
+  def should_generate_new_friendly_id?
+    name_changed? || super
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706190030) do
+ActiveRecord::Schema.define(version: 20170709041811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 20170706190030) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_cities_on_slug"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "user_id"
+    t.bigint "entry_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entry_id"], name: "index_comments_on_entry_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "entries", force: :cascade do |t|
@@ -30,7 +42,9 @@ ActiveRecord::Schema.define(version: 20170706190030) do
     t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["city_id"], name: "index_entries_on_city_id"
+    t.index ["slug"], name: "index_entries_on_slug"
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
@@ -48,6 +62,8 @@ ActiveRecord::Schema.define(version: 20170706190030) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "comments", "entries"
+  add_foreign_key "comments", "users"
   add_foreign_key "entries", "cities"
   add_foreign_key "entries", "users"
 end

f2f6d38c988e8a09957290884476bc8b0c42bb0c
nuranned <nuranned@gmail.com>
2017-07-09 08:35:59 -0700

    validation and flash messages  for entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :user
   belongs_to :city
+
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :description, presence: true
 end
